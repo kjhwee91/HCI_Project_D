7ff1d6451674b22adc38352fee092d5791093716
junggang <missjingi@gmail.com>
2014-06-26 19:06:25 +0900

    Revert "commit all"
    This reverts commit f273abd5b4c2cdb6c5b213caee0bdb0efa94c507.

--- HCI_Project_D.git/trunk/noon_noon_KinectAndServer/src/config.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-
-const float SNOW_FLOOR_HEIGHT = 120.0f;
-const float SNOWBALL1_SIZE = 470.0f;
-const float SNOWBALL2_SIZE = 300.0f;
-
-const float SNOWBALL2_INITPOS_X = 1000.0f;
-
-const float SNOWBALL1_INITPOS_X = 1000.0f;
